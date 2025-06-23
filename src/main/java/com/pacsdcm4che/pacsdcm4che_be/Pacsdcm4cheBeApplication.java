51ffdcae08887dbcc1cfe7163ae379a6b984a27e
manhnguyen <manhcan03@gmail.com>
2025-06-23 08:32:40 +0700

    start the project

---
+++ src/main/java/com/pacsdcm4che/pacsdcm4che_be/Pacsdcm4cheBeApplication.java
@@ -0,0 +1,13 @@
+package com.pacsdcm4che.pacsdcm4che_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pacsdcm4cheBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pacsdcm4cheBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pacsdcm4che/pacsdcm4che_be/Pacsdcm4cheBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacsdcm4che.pacsdcm4che_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pacsdcm4cheBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
